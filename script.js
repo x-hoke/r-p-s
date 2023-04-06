d074354e1c3cac4fe7b6df693609e5b217ef9625
x-hoke <danylosimchuk@gmail.com>
2023-04-07 00:43:05 +0300

    Game is plyable for one round

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-//const valuesArr = ["ROCK","PAPER","SCISSORS"]
-//const playerSelection ="ROCK"
-//const computerSelection = //getComputerChoise()
+const valuesArr = ["ROCK","PAPER","SCISSORS"]
+const playerSelection = prompt(`Take your choice:PAPER,ROCK,SCISSORS`)
+
+console.log(playerSelection)
+const computerSelection = getComputerChoise()
+function playerSelectionUp(playerSelection){
+    return playerSelection.toLocaleUpperCase
+}
+
 function singleRound(playerSelection,computerSelection){
     if((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection ==="ROCK")){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`)
+        console.log(`You win your ${playerSelection} beats computers ${computerSelection}`)
     }else if((playerSelection === "ROCK" && computerSelection === "PAPER") || (playerSelection ==="PAPER" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "ROCK")){
-        console.log(`You lose ${computerSelection} beats ${playerSelection}`)
+        console.log(`You lose computer picks ${computerSelection} what beats your ${playerSelection}`)
     }else if(playerSelection === computerSelection){
         console.log(`Your selection is ${playerSelection} , well, computer choose ${computerSelection} too, its draw `)
     }
    
 }
-/*function getComputerChoise(){
+function getComputerChoise(){
     const random = Math.floor(Math.random() * valuesArr.length);
-    console.log(valuesArr[random]);
+    console.log(valuesArr[random])
     return (valuesArr[random])
-    
-}*/
+        
+}
 
 
 
-singleRound("SCISSORS","SCISSORS")
+singleRound(playerSelection,computerSelection)
 
 
 
