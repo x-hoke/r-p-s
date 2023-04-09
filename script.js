d2763a385c0e16b3574f397098e06228da011c54
x-hoke <danylosimchuk@gmail.com>
2023-04-09 23:23:10 +0300

    added 5 round game and score

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
 const valuesArr = ["ROCK","PAPER","SCISSORS"]
-const playerSelection = prompt(`Take your choice:PAPER,ROCK,SCISSORS`)
+let playerWins = 0;
+let computerWins = 0;
 
-console.log(playerSelection)
-const computerSelection = getComputerChoise()
-function playerSelectionUp(playerSelection){
-    return playerSelection.toLocaleUpperCase
+function game(){
+    
+    for(let i = 0;i<5;i++){
+        const playerSelectionInitial = prompt(`Take your choice:PAPER,ROCK,SCISSORS`)
+    const playerSelection = playerSelectionInitial.toUpperCase()
+    const computerSelection = getComputerChoise()
+    singleRound(playerSelection,computerSelection)
+    console.log(`you:${playerWins}, computer :${computerWins}`)
+    }
+    
 }
+
+
+
 
 function singleRound(playerSelection,computerSelection){
     if((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection ==="ROCK")){
         console.log(`You win your ${playerSelection} beats computers ${computerSelection}`)
+        playerWins++;
     }else if((playerSelection === "ROCK" && computerSelection === "PAPER") || (playerSelection ==="PAPER" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "ROCK")){
         console.log(`You lose computer picks ${computerSelection} what beats your ${playerSelection}`)
+        computerWins++
     }else if(playerSelection === computerSelection){
         console.log(`Your selection is ${playerSelection} , well, computer choose ${computerSelection} too, its draw `)
+        
     }
    
 }
 function getComputerChoise(){
     const random = Math.floor(Math.random() * valuesArr.length);
-    console.log(valuesArr[random])
     return (valuesArr[random])
         
 }
 
 
 
-singleRound(playerSelection,computerSelection)
+game()
+if(playerWins>computerWins){
+    console.log(`Your score: ${playerWins}, Computer score: ${computerWins}, You win, Congratz!!!`)
+}else if(playerWins<computerWins){
+    console.log(`Player score: ${playerWins}, Computer score: ${computerWins}, Computer beats you, emoji:sadge`)
+}else{
+    console.log(`Its tie`)
+}
+
 
 
 
